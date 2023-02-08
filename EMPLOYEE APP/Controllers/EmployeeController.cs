36145b61580cbff20401594c49bde018a2aff43a
Sachin R <121842168+sachin17R@users.noreply.github.com>
2023-02-08 19:50:58 +0530

    Add files via upload

---
+++ EMPLOYEE APP/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeApplication.ViewModelFolder;
+using EmployeeLib.DataClasses;
+
+namespace EmployeeApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        private readonly EmployeeDataComponent repo = new EmployeeDataComponent();
+        public ActionResult Index()
+        {
+     
+            return View();
+        }
+
+        public ActionResult AllEmp()
+        {
+            var model = repo.GetAllEmployee();
+            return PartialView(model);
+        }
+        public ActionResult AddNew()
+        {
+            return PartialView(new ViewModelClass());
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(ViewModelClass model,string Alldep)
+        {
+            if (ModelState.IsValid)
+            {
+                model.deptId = int.Parse(Alldep);
+                var emp = new Employee
+                {
+                    EmpName = model.Name,
+                    Address = model.Address,
+                    Salary = model.salary,
+                    DeptId = model.deptId
+                };
+                repo.AddNewEmployee(emp);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return PartialView(model);
+            }
+        }
+
+        public ActionResult Update(string id)
+        {
+            var id1 = int.Parse(id);
+           var selected= repo.FindEmp(id1);
+            var alldep = new List<SelectListItem>();
+            var data = repo.GetAllDept();
+            foreach (var item in data)
+            {
+                var single = new SelectListItem { Text = item.DeptName, Value = item.DeptId.ToString() };
+                alldep.Add(single);
+            }
+            TempData["AllDept"] = alldep;
+            return PartialView(selected);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Employee model)
+        {
+           // model.DeptId = int.Parse(Alldep);
+            repo.UpdateEmployee(model);
+            return RedirectToAction("index");
+        }
+
+      
+        public ActionResult delete(string id)
+        {
+            var id1 = int.Parse(id);
+            repo.DeleteEmployee(id1);
+            return RedirectToAction("AllEmp");
+        }
+
+
+
+    }
+}
---
+++ EMPLOYEE APP/ViewModelFolder/ViewModelClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApplication.ViewModelFolder
+{
+    public class ViewModelClass
+    {
+        public int EmpId { get; set; }
+
+
+        [Required(ErrorMessage ="Enter Something",AllowEmptyStrings =false)]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage ="Enter Something",AllowEmptyStrings =false)]
+        [MinLength(4,ErrorMessage ="Data should Be More Than 4 chars")]
+        public string Address { get; set; }
+
+
+
+        [Range(minimum:10000,maximum:100000,ErrorMessage ="Sal Should Between The Range 10000 to 100000")]
+        public int salary { get; set; }
+
+
+        public int deptId { get; set; }
+    }
+}
